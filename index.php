bd3c291c253de0425f2ef872c48821bfb36edabe
jonathan14052001 <jonathan.nicholas.14052001@gmail.com>
2023-10-02 15:00:02 +0700

    Menambahkan List

--- index.php
+++ index.php
@@ -8,5 +8,6 @@
 <body>
     <h1>Hello World!</h1>
     <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maxime, porro saepe quisquam minus perferendis temporibus sint natus eveniet architecto consectetur. Error voluptatem molestiae iusto blanditiis impedit aliquid itaque culpa nihil.</p>
+    <li>Hi</li>
 </body>
 </html>
